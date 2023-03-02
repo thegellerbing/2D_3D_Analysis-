293e72cc07e6c31a079c100b58b1aa1ebb9be68b
thegellerbing <125418717+thegellerbing@users.noreply.github.com>
2023-03-03 06:20:14 +0800

    Update KM_func.R

--- KM_func.R
+++ KM_func.R
@@ -1,11 +1,12 @@
 tidydata = function(expr, md, hgnccolnum, genestostudy){
   cat("Combining expression data with clinical data ....\n")
-  if(exists("genestostudy")) {expr %<>% filter(expr[,hgnccolnum] %in% genestostudy)}
-  expr <- expr %>% tibble::column_to_rownames(colnames(df)[hgnccolnum])
+  if(!is.null(genestostudy)) {expr %<>% filter(expr[,hgnccolnum] %in% genestostudy)}
+  expr <- expr %>% tibble::column_to_rownames(colnames(expr)[hgnccolnum])
   if(all(sapply(expr[,1], is.character))){
     expr <- expr[, -1]
   }
   expr <- as.data.frame(t(expr))
+  rownames(expr) <- gsub("\\.", "-", rownames(expr))
   colnames(md)[1] <- 'id'
   if(!identical (rownames(expr), md$id)) {
     expr %<>% filter(rownames(expr) %in% md$id)
@@ -20,7 +21,7 @@ tidydata = function(expr, md, hgnccolnum, genestostudy){
   
   cat("Checking for NA values ....\n")
   md %<>% filter(md$os != 'NA')
-  if(all(md$os != 'NA')) {cat("Data is clean. Congrats :p \n")}
+  if(all(md$os != 'NA')) {cat("Data is clean. Congrats :p It is now safe to proceed :> \n")}
   
   md %<>%
     mutate(status = as.numeric(
@@ -44,10 +45,13 @@ KMgraph = function(df, genestostudy){
         plot.title=element_text(hjust=0.5, size = 18),
         legend.box = "vertical"
       )}
+  
+  genestostudy <- gsub("-", "", genestostudy)
+  
   for (i in unique(1:length(genestostudy)))
   {
     x = genestostudy[i]
-    df %>% dplyr::select(x) %>% unlist() -> y
+    df %>% dplyr::select(all_of(x)) %>% unlist() -> y
     mdn = median(y) 
     df %<>%
       mutate(expression = as.factor(
@@ -79,16 +83,17 @@ KMgraph = function(df, genestostudy){
 }
 
 KMunitable = function(kmdf, genestostudy){
-  at("Executing Kaplan Meier analysis in table form .... \n")
+  cat("Executing Kaplan Meier analysis in table form .... \n")
   unicolumn = c('Gene', 'Hazard Ratio', '95% CI', 'P-value')
-  unidf = data.frame(matrix(nrow=0, ncol = length(unicolumn)))
-  colnames(unidf) = unicolumn
+  uniKM = data.frame(matrix(nrow=0, ncol = length(unicolumn)))
+  colnames(uniKM) = unicolumn
+  genestostudy <- gsub("-", "", genestostudy)
   
   for (i in unique(1:length(genestostudy)))
   {
     z = genestostudy[i]
     name = paste(z, 'Expression', sep = "")
-    kmdf %>% dplyr::select(z) %>% unlist() -> c
+    kmdf %>% dplyr::select(all_of(z)) %>% unlist() -> c
     mdn = median(c)
     kmdf %<>% mutate("{name}" := as.factor(
       ifelse(
@@ -118,25 +123,29 @@ KMunitable = function(kmdf, genestostudy){
     uci <- paste(ci[1], '~', ci[2], sep = " ")
     unidf2 = data.frame(x, ucoef, uci, upvalue)
     colnames(unidf2) <- unicolumn
-    unidf <- rbind(unidf, unidf2)}
+    uniKM <- rbind(uniKM, unidf2)}
   cat("Analysis Complete :> \n")
-  return(unidf)
+  uniKM %>% filter(uniKM$`P-value` < 0.05) %>% as.data.frame() -> sigKM
+  list1 <- (list(uniKM, sigKM))
+  names(list1) <- c("uniKM", "sigKM")
+  return(list1)
 }
 
-multicox = function(kmdf, genestostudy){
+multicox = function(kmdf, siggene){
   cat("Executing Multivariate Cox Analysis ... \n")
+  siggene <- gsub("-", "", siggene)
   for(col_name in colnames(kmdf)) {
-    if(tolower(col_name) %in% tolower(genestostudy)) {
+    if(tolower(col_name) %in% tolower(siggene)) {
       colnames(kmdf)[colnames(kmdf) == col_name] <- tolower(col_name)
         }
   }
   
-  for (i in unique(1:length(genestostudy)))
+  for (i in unique(1:length(siggene)))
   {
-    z = genestostudy[i]
-    l = tolower(genestostudy[i])
+    z = siggene[i]
+    l = tolower(siggene[i])
     name = paste(z)
-    kmdf %>% dplyr::select(l) %>% unlist() -> c
+    kmdf %>% dplyr::select(all_of(l)) %>% unlist() -> c
     mdn = median(c)
     kmdf %<>% mutate("{name}" := as.factor(
       ifelse(
@@ -145,14 +154,33 @@ multicox = function(kmdf, genestostudy){
         'Low'
       )))}
   
-  for (o in genestostudy){
+  for (o in siggene){
     kmdf[[o]] <- relevel(factor(kmdf[[o]]), ref = 'Low')
   }
   
   survdf <- Surv(time = kmdf$os, event = kmdf$status)
-  f1 <- as.formula(paste('survdf ~', paste(genestostudy, collapse = "+")))
+  f1 <- as.formula(paste('survdf ~', paste(siggene, collapse = "+")))
   cox = coxph(f1, data = kmdf) %>% print()
   ggforest(cox, data = kmdf, refLabel = "Reference") %>% print()
-  cat("Analysis Complete :> \n")
+  cat("Analysis Complete :> Hope you get something :> \n")
   return(cox)
+}
+
+sigcoxph = function(cox, siggene){
+  unicolumn = c('Gene', 'Hazard Ratio', '95% CI', 'P-value')
+  sigcox = data.frame(matrix(nrow=0, ncol = length(unicolumn)))
+  
+  ucoef = summary(cox)$coef[,2]
+  upvalue = summary(cox)$coef[,5]
+  ci = confint(cox, level = 0.95)
+  ci <- exp(ci)
+  ci <- round(ci, digits = 2)
+  uci <- paste(ci[,1], '~', ci[,2], sep = " ")
+  df = data.frame(siggene, ucoef, uci, upvalue)
+  
+  sigcox <- rbind(sigcox, df)
+  colnames(sigcox) = unicolumn
+  rownames(sigcox) = NULL
+  sigcox <- sigcox[which(sigcox$`P-value` < 0.05), ]
+  return(sigcox)
 }
